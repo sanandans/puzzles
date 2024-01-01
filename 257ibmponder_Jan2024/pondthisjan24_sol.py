18e2f6b29381b49d0a3730e003c83b8de68449f5
Sanandan Swaminathan <sanandan@Vidyas-MacBook-Air.local>
2023-12-31 19:57:00 -0800

    added jan 2024 stub

---
+++ 257ibmponder_Jan2024/pondthisjan24_sol.py
@@ -0,0 +1,8 @@
+'''
+My IBM Ponder This January '24 challenge main and bonus * solutions
+https://research.ibm.com/haifa/ponderthis/challenges/January2024.html
+Sanandan Swaminathan, submitted December 31, 2023
+
+Will post my main and bonus * Jan solutions when the Jan challenge closes 
+in Feb.
+'''
