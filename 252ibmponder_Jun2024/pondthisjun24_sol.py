5925b84f7d6b168194daea5ebd3ed7dfed576be4
Sanandan Swaminathan <sanandan@Vidyas-MacBook-Air.local>
2024-05-31 19:28:23 -0700

    added June sol stub

---
+++ 252ibmponder_Jun2024/pondthisjun24_sol.py
@@ -0,0 +1,7 @@
+'''
+My IBM Ponder This June '24 challenge main and bonus * solutions (both complete instantaneously).
+https://research.ibm.com/haifa/ponderthis/challenges/June2024.html
+Sanandan Swaminathan, submitted May 31, 2024
+
+Will post my June solutions when the June challenge closes in July.
+‘’’
