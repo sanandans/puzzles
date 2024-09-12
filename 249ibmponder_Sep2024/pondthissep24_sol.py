3e97c7dba6232ad273de3701535d748d029cba8e
Sanandan Swaminathan <sanandan@Vidyas-MacBook-Air.local>
2024-09-11 17:44:34 -0700

    my Sept sol stub

--- 249ibmponder_Sep2024/pondthissep24_sol.py
+++ 249ibmponder_Sep2024/pondthissep24_sol.py
@@ -1,5 +1,5 @@
 '''
-My IBM Ponder This September '24 challenge solution
+My IBM Ponder This September '24 challenge main and bonus * solutions (both complete instantaneously)
 https://research.ibm.com/haifa/ponderthis/challenges/September2024.html
 Sanandan Swaminathan, submitted August 30, 2024
 
