b217df180d86dc4e002e8d8a02f787fc2602a018
Sanandan Swaminathan <sanandan@Vidyas-MacBook-Air.local>
2024-11-12 17:35:15 -0800

    added my Nov sol stub

---
+++ 248ibmponder_Nov2024/pondthisnov24_sol.c
@@ -0,0 +1,7 @@
+/*
+My IBM Ponder This November '24 challenge main and bonus * solutions
+https://research.ibm.com/haifa/ponderthis/challenges/November2024.html
+Sanandan Swaminathan, submitted November 12, 2024
+
+Will post my November solutions when the November challenge closes in December.
+*/
