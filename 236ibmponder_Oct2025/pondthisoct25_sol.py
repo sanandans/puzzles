32666927723c44d0c717d6dd106161524003d057
Sanandan Swaminathan <sanandan@Vidyas-MacBook-Air.local>
2025-10-21 17:26:00 -0700

    added my September sol and October sol stub

---
+++ 236ibmponder_Oct2025/pondthisoct25_sol.py
@@ -0,0 +1,7 @@
+'''
+My IBM Ponder This October '25 challenge main and bonus * solutions (both complete instantaneously)
+https://research.ibm.com/haifa/ponderthis/challenges/October2025.html
+Sanandan Swaminathan, submitted September 30, 2025
+
+Will post my October solutions when the October challenge closes in November
+'''
--- 237ibmponder_Sep2025/pondthissep25_sol.py
+++ 237ibmponder_Sep2025/pondthissep25_sol.py
@@ -3,5 +3,104 @@ My IBM Ponder This September '25 challenge main and bonus * solutions (both comp
 https://research.ibm.com/haifa/ponderthis/challenges/September2025.html
 Sanandan Swaminathan, submitted September 1, 2025
 
-Will post my September solutions when the September challenge closes in October
+In this puzzle, successive wedges are being cut, flipped, and re-fused around the cake with no gap or
+overlap between two successive cuts. A cut, flip, and re-fuse operation (all together) is a “step”
+according to the puzzle. If the wedge angle theta (T) perfectly divides 2pi, as in the given example,
+then we obviously need N = 2pi/T steps to make the whole cake non-iced on top for the first time, and
+2N steps to make the whole cake iced on top again for the first time. Clearly, it’s not as
+straightforward if T does not divide 2pi perfectly (as is the case with T = e^(-10) in the puzzle).
+With such a T, the first time we go around the circle, there will be a leftover sector (central
+angle < T) just before we cover the whole circle. When we do the next step, we would be including a
+part of the old, first sector as well apart from the leftover piece. I first used a toy example to
+discern a pattern. Suppose T covers 30% of the circle (T ‎ =  3pi/5 radians). Let the first three
+wedges  processed be T1, T2, T3 which would all be non-iced on top after three steps. We would have
+10% of the cake remaining with icing on top. Let us call this central angle P (pi/5 in this toy
+example). When we do the 4th step, it would include this remaining piece as well as angle Q = T - P
+from sector T1. The 4th step would not include T - (T - P) ‎ =  P central angle of T1. The 5th step
+would include that angle P of T1 as well as angle Q from T2, and exclude angle P of T2. The 6th step
+would include that angle P of T2 as well as angle Q of T3, and exclude angle P of T3. Then there’s the
+angle P between sectors T3 and T1. We can visualize the whole circle as consisting of sectors Q1, P1,
+Q2, P2, Q3, P3, P4, say in a clockwise direction. There are three Q sectors and four P sectors,
+including two consecutive P sectors. For a step, we can treat the radial line between consecutive P’s
+as one arm of the wedge angle, and the far radial line of the next Q in clockwise direction as the
+other arm. The wedge angle will be P + Q = T, as desired. When we cut, flip, and re-fuse this wedge,
+we will get consecutive P’s again. We can keep repeating this procedure using the radial line between
+consecutive P’s as our guide. Let 1 denote iced on top for a sector, and 0 denote non-iced on top. The
+first few steps would change states of the sectors as follows. In each state, the two consecutive P’s
+are shown first. In the beginning, all the sectors are iced on top (state 1).
+
+Step 1: P3 (1), P4 (1), Q1 (1), P1 (1), Q2 (1), P2 (1), Q3 (1) -> all sectors are iced; do the step on
+the wedge containing P4 and Q1 -> P4 (0), P1 (1), Q2 (1), P2 (1), Q3 (1), P3 (1), Q1 (0).
+Step 2: P4 (0), P1 (1), Q2 (1), P2 (1), Q3 (1), P3 (1), Q1 (0) -> do the step on the next wedge which
+begins where the previous wedge ended and contains P1 and Q2 - > P1 (0), P2 (1), Q3 (1), P3 (1), Q1 (0),
+P4 (0), Q2 (0).
+Step 3: P1 (0), P2 (1), Q3 (1), P3 (1), Q1 (0), P4 (0), Q2 (0) -> do the step on the next wedge which
+begins where the previous wedge ended and contains P2 and Q3 - > P2 (0), P3 (1), Q1 (0), P4 (0), Q2 (0),
+P1 (0), Q3 (0).
+Step 4: P2 (0), P3 (1), Q1 (0), P4 (0), Q2 (0), P1 (0), Q3 (0) -> do the step on the next wedge which
+begins where the previous wedge ended and contains P3 and Q1 - > P3 (0), P4 (0), Q2 (0), P1 (0), Q3 (0),
+P2 (0), Q1 (1).
+
+And so on.
+
+In the first three steps, we flipped three P sectors and three Q sectors. In the 4th step, we flipped the
+remaining one P sector but also flipped a Q sector a second time (back to iced). This is the result of
+having four P sectors and one less Q sector, and having the sectors in a pattern of Q, P, Q, P, Q, P, P
+at each step. Clearly, all the Q sectors are non-iced on top after 3 steps, and hence all of them will be
+iced again after 6 steps, and then all non-iced again after 9 steps, and then all iced again after 12
+steps, and so on. Similarly, all the P sectors are non-iced on top after 4 steps, and hence all of them
+will be iced again after 8 steps, and then all non-iced again after 12 steps, and then all iced again
+after 16 steps, and so on. So, Q sectors become iced again after 6, 12, 18, 24, 30... steps, and P sectors
+become iced again after 8, 16, 24, 32... steps. We can see that the whole cake (all Q and P sectors
+together) becomes iced again for the first time after 24 steps, i.e. the least common multiple of 6 and 8,
+and then again after 48, 72, 96... steps (at every common multiple of 6 and 8).
+
+We can obviously extend the procedure of the toy example above to any wedge angle T. It clearly doesn’t
+matter what the actual values of angles P and Q are as long as P is not zero (if P is 0, as in the
+example given in the puzzle, we know that we can make the whole cake iced on top again trivially in
+2N = 4pi/T steps). When P > 0, let N = floor(2pi/T). Then, all the Q sectors become iced again after 2N,
+4N, 6N... steps. And all the P sectors become iced again after 2(N+1), 4(N+1), 6(N+1)... steps. So, the
+first time the whole cake (all Q and P sectors together) becomes iced again is the LCM of 2N and 2(N+1),
+which is 2N(N+1).
+
+In the puzzle, 2pi / e^(-10) ‎ = 138396.366 approximately. So, N = 138396. The circle can be visualized as
+having sectors Q1, P1, Q2, P2… Q138396, P138396, P138397 in clockwise order. Using the procedure described
+for the toy example above, all the Q sectors will be non-iced after 138396 steps, then iced again after
+138396 x 2 steps, then non-iced again after 138396 x 3 steps, and so on. All the P sectors will be
+non-iced after 138397 steps, then iced again after 138397 x 2 steps, then non-iced again after 138397 x 3
+steps, and so on. The whole cake will become iced again for the first time after 2 x 138396 x 138397 ‎
+= 38,307,182,424 steps (the LCM of 138396 x 2 and 138397 x 2).
+
+For the bonus puzzle, we can see that the whole cake can NEVER become fully non-iced (all icing on bottom).
+The Q sectors all become non-iced after N, 3N, 5N... steps. And the P sectors all become non-iced after
+N+1, 3(N+1), 5(N+1)... steps. Clearly, there is no number common between these two infinite sets of
+numbers. If N is even, as in this puzzle, then all the numbers in the first set are even, and all the
+numbers in the second set are odd. Similarly, if N was odd, then all the numbers in the first set would
+be odd, and all the numbers in the second set would be even. The whole cake can reach “non-iced on top”
+state (all icing on bottom) only if the wedge angle theta perfectly divides 2pi, as in the example given
+in the puzzle.
+
+This approach works for any wedge angle T, 0 < T < 2pi. If N = 2pi/T is an integer, then the whole cake
+can become iced again on top for the first time after 2N steps (and fully non-iced on top for the first
+time after N steps). If N is not an integer, then the whole cake can become iced again on top for the
+first time after 2N(N+1) steps but can never become fully non-iced on top. In particular, if pi < T < 2pi,
+the whole cake can become iced again on top for the first time after 2 x 1 * 2 ‎ = 4 steps (and can never
+become fully non-iced on top).
+
+Though no coding was needed for this puzzle, I wrote a short code snippet below to compute the number of
+steps to make the whole cake iced again on top for the first time for any given wedge angle theta
+(0 < theta < 2pi), and the number of steps to make the whole cake non-iced on top for the first time if
+theta perfectly divides 2pi. To specify an angle that perfectly divides 2pi, express it in terms of pi
+below rather than a number.
 '''
+
+import math
+THETA = math.e**(-10) #the desired theta angle
+#THETA = math.pi/2 #the theta angle in the example
+num_q_sectors = int(2*math.pi/THETA)
+if num_q_sectors*THETA == 2*math.pi:
+    print("Min num steps to flip whole cake to iced again:", 2*num_q_sectors)
+    print("Min num steps to flip whole cake to non-iced:", num_q_sectors)
+else:
+    print("Min num steps to flip whole cake to iced again:", 2 * num_q_sectors * (num_q_sectors+1))
+    print("Whole cake cannot be flipped to non-iced")
